e19d85d604244fac06488283993bc0cb913b8e05
EmmanuellDev <emmanuelsk04@gmail.com>
2025-04-06 21:04:49 +0530

    faqView completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,12 +25,12 @@ const Home: NextPage = (props) => {
        <OfferView />
       <FaqView />
 
-      {/* {openCreateModal && (
+       {openCreateModal && (
         <div className='new_loader relative h-full bg-slate-900'>
           <CreateView setOpenCreateModal={setOpenCreateModal} />
         </div>
       )}
-      {openTokenMetaData && (
+      {/* {openTokenMetaData && (
         <div className='new_loader relative h-full bg-slate-900'>
           <TokenMetadata setOpenTokenMetaData={setOpenTokenMetaData} />
         </div>
