0edd5532a1d85182991fb3f3a99582b6ab7fbaad
EmmanuellDev <emmanuelsk04@gmail.com>
2025-04-08 18:26:49 +0530

    token creation function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage = (props) => {
           <TokenMetadata setOpenTokenMetaData={setOpenTokenMetaData} />
         </div>
       )}
-      {openContact && (
+       {openContact && (
         <div className='new_loader relative h-full bg-slate-900'>
           <ContactView setOpenContact={setOpenContact} />
         </div>
--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -13,7 +13,7 @@ import CreateSVG from "../../components/SVG/CreateSVG";
 import Branding from 'components/Branding';
 import { InputView } from "../index";
 
-export const CreateView: FC = ({ setOpenCreateModel }) => {
+export const CreateView: FC = ({ setOpenCreateModal }) => {
   const { connection } = useConnection();
   const { publickey, sendTransaction } = useWallet;
   const { NetworkConfiguration } = useNetworkConfiguration();
@@ -221,12 +221,71 @@ export const CreateView: FC = ({ setOpenCreateModel }) => {
                   </label> )
                 }
               </div>
+              <textarea rows='6' onChange={(e) => handleFormFieldChange("description", e)} className='border-default-200 relative mt-48 block w-full rounded border-white/10 bg-transparent py-1.5 px-3 text-white/80 focus:border-white/25 focus:ring-transparent' placeholder='description of your token'>
+              </textarea>
+            </div>
+            <div className='lg:ps-0 flex flex-col p-10'>
+              <div className='pb-6 my-auto'>
+                <h4 className='mb-4 text-2xl font-bold text-white'>Solana Token Creator</h4>
+                <p className='text-default-300 mb-8 max-w-sm'>Kindly provide all the details about your token</p>
+                <div className='text-start'>
+                  <InputView name="Name" placeholder="name" clickhandle={(e) => handleFormFieldChange("name", e)} />
+                  <InputView name="Symbol" placeholder="symbol" clickhandle={(e) => handleFormFieldChange("symbol", e)} />
+                  <InputView name="Decimals" placeholder="decimals" clickhandle={(e) => handleFormFieldChange("decimals", e)} />
+                  <InputView name="Amount" placeholder="amount" clickhandle={(e) => handleFormFieldChange("amount", e)} />
+                  <div className='mb-6 text-center'>
+                    <button onClick={() => createToken(token)} className='bg-primary-600/90 hover:bg-primary-600 group mt-5 inline-flex w-full items-center justify-center rounded-lg px-6 py-2 text-white backdrop-blur-2xl transition-all duration-500' type='sumbit'>
+                      <span className='fw-bold'>Create Token</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className='text-center'>
+                  <ul className='flex flex-wrap items-center justify-center gap-2'>
+                    <li>
+                      <a onClick={() => setOpenCreateModal(false)} className='group inline-flex h-10 w-10 items-center justify-center rounded-lg bg-white/20 backdrop-blur-2xl transition-all duration-500 hover:bg-blue-600/60'>
+                        <i className='text-2xl text-white group-hover:text-white'>
+                          <AiOutlineClose />
+                        </i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
           </div>
         </div>
     </section>
-  ) : ("")}
+  ) : (
+    <section className='flex w-full items-center py-6 px-0 lg:h-screen lg:p-10'>
+    <div className='container'>
+      <div className='bg-default-950/40 mx-auto max-w-5xl overflow-hidden rounded-2xl backdrop-blur-2xl'>
+      <Branding image='auth-img' title="To Build your solana token Creator" message="Try and create your first ever solana project, and if you want to master blockchain development then check the course" />
+      <div className='lg:ps-0 flex h-full flex-col p-10'>
+        <div className='pb-10'>
+          <a className='flex'>
+            <img src='assets/images/logo1.png' alt='logo' className='h-10' />
+          </a>
+        </div>
+        <div className='my-auto pb-6 text-center'>
+          <h4 className='mb-4 text-2xl font-bold text-white'>Link to your new token</h4>
+          <p className='text-default-300 mx-auto mb-5 max-w-sm'>Your Solana token is Successfully created, check now explorer</p>
+          <div className='flex items-start justify-center'>
+            <img src={token.image || "assets/images/logo1.png"} alt='' className='h-20' />
+            <div className='mt-5 w-full text-center'>
+              <p className='text-default-300 text-base font-medium leading-6'>
+                <InputView name={"Token Address"} placeholder={tokenMintAddress} />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+</section>
+  )}
     </>
   )
 };
